3e18b862097689be059bea0caca3c0958526e554
wangzhongjie <wangzhongjie@neuxt.com>
2025-03-15 17:07:25 +0800

    Auto commit at 2025-03-15 17:07:25

--- 19.pip.py
+++ 19.pip.py
@@ -1,1 +1,11 @@
 # 包管理工具 pip 的使用
+# 安装类型提示包
+# pip install types-requests
+import requests
+
+# Type-annotated response
+resp: requests.Response = requests.get(
+    "http://api.tianapi.com/guonei/?key=APIKey&num=10"
+)
+if resp.status_code == 200:
+    print(resp.json())
